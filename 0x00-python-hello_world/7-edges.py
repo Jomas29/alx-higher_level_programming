7b7833b14025d6d328f64bffac3ce48f1cfd91e0
Jomas <jomas_29@yahoo.com>
2023-11-30 00:04:52 +0100

    7-edges corrected

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3, *middle_word, word_last_2 = word
-word_first_3 = ''.join(word_first_3[:3])
-word_last_2 = ''.join(word_last_2[-2:])
-middle_word = ''.join(middle_word)
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")

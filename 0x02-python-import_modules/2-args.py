c3ad6b8f8cc384626ebd4a0fc04e9cfd33137ed3
Jomas <jomas_29@yahoo.com>
2023-12-01 21:36:47 +0100

    print list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    count = 1
+if argc == 0:
+    print("0 arguments.")
+elif argc == 1:
+    print("1 argument:")
+    print("1: ",sys.argv[1])
+else:
+    print("{} arguments:".format(argc))
+    while count <= argc:
+        print("{}: ".format(count),sys.argv[count])
+        count += 1

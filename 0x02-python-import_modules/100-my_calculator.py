34f8810d93e98de889d32ecc9a6eed4a4ef1d983
Jomas <jomas_29@yahoo.com>
2023-12-02 00:55:27 +0100

    more advanced tasks

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+a, op, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+
+if op == '+':
+    result = add(a, b)
+elif op == '-':
+    result = sub(a, b)
+elif op == '*':
+    result = mul(a, b)
+elif op == '/':
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print(f"{a} {op} {b} = {result}")

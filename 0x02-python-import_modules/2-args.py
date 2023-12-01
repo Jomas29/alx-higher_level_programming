ec4037ebcad20d2fbef88dd0ee90915b43d7d067
Jomas <jomas_29@yahoo.com>
2023-12-01 23:25:18 +0100

    list args corrected for correct output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,14 @@
 import sys
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
-    count = 1
 if argc == 0:
     print("0 arguments.")
 elif argc == 1:
     print("1 argument:")
-    words = sys.argv[count].split()
+    words = sys.argv[1].split()
     for i, word in enumerate(words, 1):
-        print("[]: {}".format(i, word))
+        print("{}: {}".format(i, word))
 else:
     print("{} arguments:".format(argc))
-    while count <= argc:
-        print("{}:".format(count), sys.argv[count])
-        count += 1
+    for i in range(1, argc + 1):
+        print("{}:".format(i), sys.argv[i])

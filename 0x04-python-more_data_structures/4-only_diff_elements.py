fbcd65663e48f86971dabc665cb6ee93d4a45a10
Jomas <jomas_29@yahoo.com>
2023-12-06 16:03:38 +0100

    print only different elements done

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/phython3
 def only_diff_elements(set_1, set_2):
-    only_diff_elements = set_1.symmetric_difference(set_2)
+    only_diff_elements = set(set_1).symmetric_difference(set(set_2))
     return list(only_diff_elements)
